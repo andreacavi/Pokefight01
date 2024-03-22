f58c0922cd9548405f93f10eb391ac7a72057d4f
Moody458 <morentrop@gmail.com>
2024-03-22 14:29:29 +0100

    added link to single Pokemon from pokedex

--- src/Components/Pokedex.jsx
+++ src/Components/Pokedex.jsx
@@ -1,6 +1,7 @@
 import { useContext, useState } from "react";
 import { PokemonAPIContext } from "../Context/PokemonAPIContext";
 import styles from "../Styles/Pokedex.module.css";
+import { Link } from "react-router-dom";
 
 export default function Pokedex() {
   const { allPokemons, loading } = useContext(PokemonAPIContext);
@@ -20,62 +21,64 @@ export default function Pokedex() {
       ) : (
         <div className={styles.allcardscontainer}>
           {allPokemons.map((pokemon, index) => (
-            <div
-              className={`${styles.card} ${
-                hoveredCard === index ? styles.flipped : ""
-              }`}
-              key={pokemon.id}
-              onMouseEnter={() => handleHover(index)}
-              onMouseLeave={() => handleHover(null)}
-            >
-              <div className={styles.cardInner}>
-                <div className={styles.cardFront}>
-                  <img
-                    style={{ width: "150px", height: "150px" }}
-                    className={styles.pokemon_pic}
-                    src={pokemon.url}
-                    alt={pokemon.name.english}
-                  />
-                  <h3 className="PokemonName">{pokemon.name.english}</h3>
-                  {pokemon.type.map((type) => (
-                    <div
-                      key={type}
-                      className="PokemonType"
-                      style={{
-                        width: "100%",
-                        display: "flex",
-                        flexDirection: "column",
-                        flexWrap: "nowrap",
-                      }}
-                    >
-                      <img
-                        style={{ height: "auto", maxWidth: "8rem" }}
-                        src={`/types/${type}.png`}
-                        alt={type}
-                      />
-                    </div>
-                  ))}
-                </div>
-                <div className={styles.cardBack}>
-                  <img
-                    style={{ width: "150px", height: "150px" }}
-                    className={styles.pokemon_pic}
-                    src={pokemon.url}
-                    alt={pokemon.name.english}
-                  />
+            <Link key={pokemon.id} to={`/pokedex/${pokemon.id}`}>
+              <div
+                className={`${styles.card} ${
+                  hoveredCard === index ? styles.flipped : ""
+                }`}
+                key={pokemon.id}
+                onMouseEnter={() => handleHover(index)}
+                onMouseLeave={() => handleHover(null)}
+              >
+                <div className={styles.cardInner}>
+                  <div className={styles.cardFront}>
+                    <img
+                      style={{ width: "150px", height: "150px" }}
+                      className={styles.pokemon_pic}
+                      src={pokemon.url}
+                      alt={pokemon.name.english}
+                    />
+                    <h3 className="PokemonName">{pokemon.name.english}</h3>
+                    {pokemon.type.map((type) => (
+                      <div
+                        key={type}
+                        className="PokemonType"
+                        style={{
+                          width: "100%",
+                          display: "flex",
+                          flexDirection: "column",
+                          flexWrap: "nowrap",
+                        }}
+                      >
+                        <img
+                          style={{ height: "auto", maxWidth: "8rem" }}
+                          src={`/types/${type}.png`}
+                          alt={type}
+                        />
+                      </div>
+                    ))}
+                  </div>
+                  <div className={styles.cardBack}>
+                    <img
+                      style={{ width: "150px", height: "150px" }}
+                      className={styles.pokemon_pic}
+                      src={pokemon.url}
+                      alt={pokemon.name.english}
+                    />
 
-                  <div>
-                    <h3 className="PokemonName">Stats:</h3>
-                    <div className="StatPs">
-                      <p>Attack:{pokemon.base.Attack}</p>
-                      <p>Defense:{pokemon.base.Defense}</p>
-                      <p>Health:{pokemon.base.HP}</p>
-                      <p>Speed:{pokemon.base.Speed} </p>
+                    <div>
+                      <h3 className="PokemonName">Stats:</h3>
+                      <div className="StatPs">
+                        <p>Attack:{pokemon.base.Attack}</p>
+                        <p>Defense:{pokemon.base.Defense}</p>
+                        <p>Health:{pokemon.base.HP}</p>
+                        <p>Speed:{pokemon.base.Speed} </p>
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
+            </Link>
           ))}
         </div>
       )}
--- src/Components/PokemonPage.jsx
+++ src/Components/PokemonPage.jsx
@@ -1,8 +1,80 @@
-import { useContext } from "react";
-import { PokemonAPIContext } from "../Context/PokemonAPIContext";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 export default function PokemonPage() {
-  const { allPokemons, loading } = useContext(PokemonAPIContext);
+  const [loading, setLoading] = useState(true); // Initialize loading state as true
+  const [singlePokemon, setSinglePokemon] = useState(null); // Change initial state to null
+  const { id } = useParams();
 
-  return <div>hello im PokemonPage</div>;
+  const fetchAPI = async () => {
+    try {
+      const response = await fetch(
+        `https://pokenode-56qg.onrender.com/pokemon/${id}`
+      );
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const data = await response.json();
+      console.log("transmitted data:", data.data);
+      setSinglePokemon(data.data);
+      setLoading(false); // Set loading to false after data is fetched
+    } catch (error) {
+      console.error("Error fetching Pokemons:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchAPI();
+  }, [id]);
+
+  if (loading) {
+    return <div>Loading Pokemons...</div>;
+  }
+
+  return (
+    <div style={{ display: "flex", flexDirection: "row" }}>
+      <img
+        style={{ width: "500px", height: "auto" }}
+        src={singlePokemon.url}
+        alt={singlePokemon.name.english}
+      />
+      <h3 className="singlePokemonName">{singlePokemon.name.english}</h3>
+      <div
+        className="PokemonType"
+        style={{
+          width: "100%",
+          display: "flex",
+          flexDirection: "column",
+          flexWrap: "nowrap",
+        }}
+      >
+        {/* Render types */}
+        {singlePokemon.type.map((type, index) => (
+          <img
+            key={index}
+            style={{ height: "auto", maxWidth: "8rem" }}
+            src={`/types/${type}.png`}
+            alt={type}
+          />
+        ))}
+        <div
+          style={{
+            width: "100%",
+            display: "flex",
+            flexDirection: "row",
+            flexWrap: "nowrap",
+          }}
+        >
+          <div>
+            <h3 className="singlePokemonName">Stats:</h3>
+            {/* Check if singlePokemon.base exists before accessing its properties */}
+            <p>Attack: {singlePokemon.base && singlePokemon.base.Attack}</p>
+            <p>Defense: {singlePokemon.base && singlePokemon.base.Defense}</p>
+            <p>Health: {singlePokemon.base && singlePokemon.base.HP}</p>
+            <p>Speed: {singlePokemon.base && singlePokemon.base.Speed}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
