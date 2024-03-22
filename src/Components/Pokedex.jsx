b9db996a24dff408274711f667b2f35b3653b3cb
Moody458 <morentrop@gmail.com>
2024-03-22 12:37:58 +0100

    card flip, type icons

--- src/Components/Pokedex.jsx
+++ src/Components/Pokedex.jsx
@@ -1,44 +1,79 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { PokemonAPIContext } from "../Context/PokemonAPIContext";
 import styles from "../Styles/Pokedex.module.css";
 
 export default function Pokedex() {
   const { allPokemons, loading } = useContext(PokemonAPIContext);
+  const [hoveredCard, setHoveredCard] = useState(null);
+
+  const handleHover = (index) => {
+    setHoveredCard(index);
+  };
+
   return (
     <div className={styles.main_pokedex_wrapper_container}>
-      {loading ? ( // Check if loading state is true
+      {loading ? (
         <div className={styles.pokedex_container}>
           <img src="Loading.gif" />
           <h1 style={{ marginTop: "2rem" }}>Loading...</h1>
         </div>
       ) : (
         <div className={styles.allcardscontainer}>
-          {allPokemons.map((pokemon) => (
-            <div className={styles.card_container} key={pokemon.id}>
-              <img
-                style={{ width: "150px", height: "150px" }}
-                className={styles.pokemon_pic}
-                src={pokemon.url}
-                alt={pokemon.name.english}
-              />
-              <div className={styles.card_back}>BACK</div>
-              <div className={styles.card_front}></div>
-              <h3 className="PokemonName">{pokemon.name.english}</h3>
-              <div className="PokemonTypes">
-                {pokemon.type.map((type) => (
-                  <>
-                    {/* <p key={type} className="PokemonType">
-                      {type}
-                    </p> */}
-                    <img
+          {allPokemons.map((pokemon, index) => (
+            <div
+              className={`${styles.card} ${
+                hoveredCard === index ? styles.flipped : ""
+              }`}
+              key={pokemon.id}
+              onMouseEnter={() => handleHover(index)}
+              onMouseLeave={() => handleHover(null)}
+            >
+              <div className={styles.cardInner}>
+                <div className={styles.cardFront}>
+                  <img
+                    style={{ width: "150px", height: "150px" }}
+                    className={styles.pokemon_pic}
+                    src={pokemon.url}
+                    alt={pokemon.name.english}
+                  />
+                  <h3 className="PokemonName">{pokemon.name.english}</h3>
+                  {pokemon.type.map((type) => (
+                    <div
+                      key={type}
+                      className="PokemonType"
                       style={{
-                        height: "3.5rem",
-                        width: "auto",
+                        width: "100%",
+                        display: "flex",
+                        flexDirection: "column",
+                        flexWrap: "nowrap",
                       }}
-                      src={`/types/${type}.png`}
-                    />{" "}
-                  </>
-                ))}
+                    >
+                      <img
+                        style={{ height: "auto", maxWidth: "8rem" }}
+                        src={`/types/${type}.png`}
+                        alt={type}
+                      />
+                    </div>
+                  ))}
+                </div>
+                <div className={styles.cardBack}>
+                  <img
+                    style={{ width: "150px", height: "150px" }}
+                    className={styles.pokemon_pic}
+                    src={pokemon.url}
+                    alt={pokemon.name.english}
+                  />
+
+                  <div>
+                    <h3 className="PokemonName">Stats:</h3>
+                    <div className="StatPs">
+                      <p>Attack:{pokemon.base.Attack}</p>
+                      <p>Defense:{pokemon.base.Defense}</p>
+                      <p>Health:{pokemon.base.HP}</p>
+                      <p>Speed:{pokemon.base.Speed} </p>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           ))}
