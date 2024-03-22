bda798d8d07bd9698ce6c24bc9471528019767a6
Andrea Caviglia <andrea.caviglia@yahoo.com>
2024-03-22 10:52:38 +0100

    pokemon page progress

--- src/Components/Error.jsx
+++ src/Components/Error.jsx
@@ -1,6 +1,10 @@
-
-
+import PokemonPage from "./PokemonPage";
 
 export default function Error() {
-  return <div>hello im Error Page</div>;
+  return (
+    <div>
+      <PokemonPage />
+      hello im Error Page
+    </div>
+  );
 }
--- src/Components/PokemonPage.jsx
+++ src/Components/PokemonPage.jsx
@@ -1,8 +1,55 @@
 import { useContext } from "react";
 import { PokemonAPIContext } from "../Context/PokemonAPIContext";
+import styles from "../Styles/PokemonPage.module.css";
+import ProgressBar from "react-bootstrap/ProgressBar";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function PokemonPage() {
-  const { allPokemons, loading } = useContext(PokemonAPIContext);
+  const { allPokemons } = useContext(PokemonAPIContext);
 
-  return <div>hello im PokemonPage</div>;
+  const prevPokemon = () => {
+    setCurrentPokemon((prev) => {
+      if (prev === 0) {
+        return allPokemons.length - 1;
+      } else {
+        return prev - 1;
+      }
+    });
+  };
+
+  const nextPokemon = () => {
+    setCurrentPokemon((prev) => {
+      if (prev === allPokemons.length - 1) {
+        return 0;
+      } else {
+        return prev + 1;
+      }
+    });
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.upperPart}>
+        <a className={styles.arrow} href="/" onClick={prevPokemon}>
+          <img src="/ArrowLeft.png" alt="Previous Pokemon" />
+        </a>
+        <div className={styles.PokemonNameContainer}>
+          <h1 className={styles.Pokename}>Bulbasaur</h1>
+          <img src="/Pika.png" alt="Pokemon" className={styles.image} />
+        </div>
+        <a className={styles.arrow} href="/" onClick={nextPokemon}>
+          <img src="/arrowRight.png" alt="Next Pokemon" />
+        </a>
+      </div>
+      <div className={styles.downPart}>
+        <div>
+          <ProgressBar variant="success" now={80} label="40%" max={120} />
+          <ProgressBar variant="info" now={20} label="20%" />
+          <ProgressBar variant="warning" now={60} label="60%" />
+          <ProgressBar variant="danger" now={80} label="80%" />
+        </div>
+        {/* This should be the white part with all the infos */}
+      </div>
+    </div>
+  );
 }
