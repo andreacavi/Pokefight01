54d0365e8c47878d9f7aeaad0285ad58b1c69a64
Andrea Caviglia <andrea.caviglia@yahoo.com>
2024-03-23 18:54:59 +0100

    added changes to pokepage

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,6 +1,8 @@
+import styles from "../Styles/Footer.module.css";
+
 export default function Footer() {
   return (
-    <div>
+    <div className={styles.container}>
       PokeFight | Group 01 | Marco, Andrea, Moritz | WBS Cooding School 2023
     </div>
   );
